603597ce42731f0a0e47f6f2e0cdc8e91aad74d1
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-21 14:48:11 -0700

    task 2- all letters in lower case and on new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the entire alphabet on a new line and in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+	for lc = 'a'; lc <= 'z'; lc++
+	{
+		putchar(lc);
+	}
+	putchar('\n);
+	return (0);
+}
