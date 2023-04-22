7bed84fae8f554ef591353e70c878a21b371312d
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-21 17:35:21 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,17 +5,18 @@
  */
 int main(void)
 {
-        int num;
+	int num, num1;
 
-        for (num = 18; num < 28; num++)
-        {
-                putchar(num);
-                if (num != 1:7)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-                putchar('\n');
-                return (0);
+	for (num = 0; num < 9; num++)
+	{
+		for (num1 = 0; num1 < 9; num1++)
+		{
+			putchar(num + '0');
+			putchar(num1 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n')
+	return (0);
 }
