a37c0c86fb88c96685461fd395faad93e700c1b4
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-25 13:07:48 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main- prints the alphabet in lower case + /n
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
