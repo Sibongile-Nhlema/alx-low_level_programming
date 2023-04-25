c315a0d23519af7b9cad85bc1519bff8b84ed1ad
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-25 14:28:34 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number.
+ * @n: is the number
+ * Return: 1 if n > 0, 0 if n = 0, -1 if n < 0
+ */
+int print_sign(int n)
+{
+	for (n >= 0)
+		for (n = 0)
+			return (0);
+		else
+			return (1);
+	else
+		return (-1);
+}
