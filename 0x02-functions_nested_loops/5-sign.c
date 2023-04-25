2add36a66ea964f0e55db7266d9bfa3d0e90f0ff
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-25 14:36:08 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,15 +6,18 @@
  */
 int print_sign(int n)
 {
-	for (n >= 0;i)
+	for (n >= 0;)
 	{
 		for (n = 0;)
 		{
+			_putchar('0');
 			return (0);
 		else
+			_putchar('+');
 			return (1);
 		}
 	else
+		_putchar('-');
 		return (-1);
 	}
 }
