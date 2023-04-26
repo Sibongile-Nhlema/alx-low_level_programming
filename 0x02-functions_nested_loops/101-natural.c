b40c83497970d2ed98f4197269a0801c4cfff695
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-26 07:44:55 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,13 @@
 int main(void)
 {
 	int i;
-	int sum = 0;
+	int sum = i0;
 
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
-			sum += 1;
+			sum += 1024;
 		}
 	}
 	printf("%d\n", sum);
