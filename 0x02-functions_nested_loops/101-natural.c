6276551588f8a54625b09d589be61173e4c6cc0b
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-26 07:30:13 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *Return: multiples of 3 and 5 until 1024
+ */
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += 1;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
