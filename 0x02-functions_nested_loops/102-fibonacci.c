e76be8e841de5489dbaef22185d2811596b1ac6e
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-26 09:15:08 -0700

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,15 +12,15 @@ int main(void)
 	fib[0] = 1;
 	fib[1] = 2;
 
-	for (i = 2; i < n; i++)
+	for (i = 2; i < N; i++)
 	{
 		fib[i] = fib[i-1] + fib[i-2];
 	}
 
-	for (i = 0;i < n; i++)
+	for (i = 0;i < N; i++)
 	{
 		printf("%d", fib[i]);
-		if (i < n-1)
+		if (i < N-1)
 		{
 			printf(", ");
 		}
