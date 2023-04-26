343055e4a0f2504beca177c1a4b98ddbe3d0f404
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-25 17:24:01 -0700

    task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_times_table - prints the n times table, starting with 0.
+ *@n: number
+ *Return: return n if n>15, n<0. else nothing
+ */
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		return (0);
+	}
+
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
+		{
+			int valur = i * j;
+
+			printf("%4d", value);
+		}
+		printf("\n");
+	}
+}
