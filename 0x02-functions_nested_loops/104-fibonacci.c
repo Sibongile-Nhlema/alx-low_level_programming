5279df640db425334e71d189433d3094bf1d17bb
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-26 12:17:08 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - prints first 98 fibonacci numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count = 3;
+	long int fib1 = 1, fib2 = 2;
+	long int ans = fib1 + fib2;
+	
+	printf("%lu, ", fib1);
+	printf("%lu, ", fib2);	
+	while (count <= 98)
+	{
+		if (count == 50)
+		{
+			printf("%lu\n", ans);
+		}
+		else
+		{
+			printf("%lu, ", ans);
+		}
+		fib1 = fib2;
+		fib2 = ans;
+		ans = fib1 + fib2;
+		count++;
+	}
+	return (0);
+}
