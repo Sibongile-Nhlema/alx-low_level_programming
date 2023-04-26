74f862966257ee991fee6bd5904c6b2e54f4d049
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-26 08:40:42 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints first 50 fibonacci numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, n =50;
+	int fib[n];
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (i =2; i < n; i++)
+	{
+		fib[i] = fib[i-1] + fib[1-2];
+	}
+
+	for (i = 0;i < n; i++)
+	{
+		printf("%d", fib[i]);
+		if (i < n-1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
