fb4a71006866af85688ebd5359be4a9be10bb83f
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-27 14:29:03 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/***
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ * Return: Always 0
+ */
+
+void more_numbers(void)
+{
+	int i = 0, num;
+
+	while (i < 10)
+	{
+		num = 0;
+
+		while (num <= 14)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+			num++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
