554813133165901665143fbf3c53865ccf8dd55e
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-27 12:47:09 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,10 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num >= 48; num <= 57; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (num != 50 && num != 54)
-			return (0);
-		_putchar(num);
+		if (num != 2 && num != 4)
+			_putchar(num);
 	}
 	_putchar('\n');
 }
