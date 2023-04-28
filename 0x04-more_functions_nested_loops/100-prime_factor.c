defe7eda453f42b4733c6abb24733ed3b039e590
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-28 10:50:18 -0700

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints largest prime number in 612852475143
+ * Return: Always 
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int i;
+
+	for (i = 2; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			i--;
+		}
+	}
+	printf("%ld\n", i);
+	return (0);
+}
