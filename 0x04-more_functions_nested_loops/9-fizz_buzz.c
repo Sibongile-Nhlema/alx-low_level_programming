2abf880dbd797f84fa8933327664c260d25a08fe
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-04-30 12:03:17 -0700

    task 9. corrections

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,27 +11,30 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 15) == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((i % 5) == 0)
-		{
-			printf("Buzz ");
-			if (i == 100)
+			if ((i % 15) == 0)
+			{
+				printf("FizzBuzz");
+				printf(" ");
+			}
+			else if ((i % 3) == 0)
+			{
+				printf("Fizz");
+				printf(" ");
+			}
+			else if ((i % 5) == 0)
 			{
 				printf("Buzz");
-				break;
+				printf(" ");
 			}
-		}
-		else
-		{
-			printf("%d ", i);
-		}
+			else if (i == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("%d", i);
+				printf(" ");
+			}
 	}
 	printf("\n");
 	return (0);
