b8580d6993f54fd0fdd8b0cf26c5b0226f9f6024
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-04 10:32:44 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,13 +5,12 @@
  * @s2: string 2
  * Return: difference between s1 and s2
  */
-
-int _strcmp(char *s1, char *s2) 
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
-	return *(unsigned char*)s1 - *(unsigned char*)s2;
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
