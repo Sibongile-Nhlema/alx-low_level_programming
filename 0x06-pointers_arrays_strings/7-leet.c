d7b3f840bbd1573677bbc8e3a0c3b7b5b254fb7b
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-04 14:43:11 -0700

    task 7 to be completed.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *leet - function that encodes a string into 1337.
+ @n: input
+Return: value of n
+*/
+
+char *leet(char *)
+{
+	int i, j;
+	
+	char s1[] = "aAeoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+	}
+}
