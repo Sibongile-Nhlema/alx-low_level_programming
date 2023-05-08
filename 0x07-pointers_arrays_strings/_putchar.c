a9bebebcd3147fb5cbb51861982b15aff8530b61
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-08 05:25:51 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the char c to stdout
+ * @c: the char to print
+ * Return: Success 1
+ * On error: -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, %c, 1));
+}
