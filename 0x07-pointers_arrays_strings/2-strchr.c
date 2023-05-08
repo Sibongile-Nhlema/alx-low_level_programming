661494ab19c3573691686a0638b2503dc7eda3f0
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-08 05:56:25 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr -  locates a character in a string
+ * @s: input
+ * @c: input
+ * Return: Always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
