8044fe6a0dff8e84ca0fd73cb86d4d817721a095
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-10 09:50:36 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <stddef.h>
 
 /**
- * _sqrt_recursion -  returns the natural square root of a number
+ *  _sqrt_recursion_helper - checks if the input integer is negative
  * @n: interger
  * @i: iterration
  * Return: -1 if there isn't a natural num, else 0
@@ -19,6 +21,13 @@ int _sqrt_recursion_helper(int n, int i)
 	}
 	return (_sqrt_recursion_helper(n, i + 1));
 }
+
+/**
+ * _sqrt_recursion -  returns the natural square root of a number
+ * @n: interger
+ * @i: iterrator
+ * Return: same as prevoius function
+ */
 
 int _sqrt_recursion(int n)
 {
