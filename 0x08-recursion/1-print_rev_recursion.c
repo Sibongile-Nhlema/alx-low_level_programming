f8e3dab565fb021abce01cccae5f2f2256236d2d
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-10 08:39:20 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion(char *s) - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: string
  * Return: reversed string
  */
