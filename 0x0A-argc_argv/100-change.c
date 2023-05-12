0cf41009356a7c193cb166fae32f4e7f523d5950
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-12 15:32:45 -0700

    task last one. Preparation

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  minimum number of coins to make change
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *  Return: 0 on success, 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
