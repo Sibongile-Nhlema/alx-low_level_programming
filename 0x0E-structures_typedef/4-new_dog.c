f685794d12c5843901668b4f507f00b9854308eb
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-22 09:27:27 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,78 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen _ returns string length
+ * @str: string
+ * Return: length of string
+ */
+
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * *_strcpy - copies string to src
+ * @dest: pointer to buffer
+ * @src: string to be copied
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, const char *src)
+{
+	char *temp = dest;
+
+	while (*src != '\0')
+	{
+		*dest++ = *src++;
+	}
+	*dest = '\0';
+	return (temp);
+}
+
+/**
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
+ * Return: pointer, otherwise NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog = malloc(sizeof(dog_t));
+
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+	newDog->name = malloc(_strlen(name) + 1);
+
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+
+	newDog->owner = malloc(_strlen(owner) + 1);
+
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+
+	_strcpy(newDog->name, name);
+	strcpy(newDog->owner, owner);
+	newDog->age = age;
+	return (newDog);
+}
