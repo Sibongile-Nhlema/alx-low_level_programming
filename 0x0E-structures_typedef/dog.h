ba518df1a71d77edf2fceacb009623ee81469ea2
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-23 09:50:45 -0700

    corrections to dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdlib.h>
 
 /**
  * struct dog - has name, age and owner info of dog
@@ -16,10 +17,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
