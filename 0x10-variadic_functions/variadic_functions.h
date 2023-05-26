2e1a13872b1cf34b5d0b311440cbd00bbd470080
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-05-26 11:42:56 -0700

    corrections on header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VARIADIC_H_
-#define _VARIABIC_H_
+#ifndef _VARIADIC_FUNCTION_H_
+#define _VARIADIC_FUNCTION_H_
 
 void print_all(const char * const format, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
