fe55e71cba6b2dbc8849776e76bfee51689b48ee
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-06-06 11:12:05 -0700

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * pop_listint - returns the nth node of a listint_t linked list
+ * @head: pointer to 1st element in linked list
+ *
+ * Return: data inside deleted element or 0
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *node = head;
+
+	for (i = 0; i < index && node != NULL; i++)
+	{
+		node = node->next;
+	}
+	return (node);
+}
