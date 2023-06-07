d7ece84e3c7af6f2ccd0cf57a027a24b0ffd1d3a
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-06-07 10:09:18 -0700

    task11

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,17 +13,24 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *current = head;
+	const listint_t *visited[1024] = {NULL};
 
 	while (current != NULL)
 	{
+		size_t i;
+
+		for (i = 0; i < count; i++)
+		{
+			if (visited[i] == current)
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				return (count);
+			}
+		}
+		visited[count] = current;
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
 
-		if (current <= current->next)
-		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
-		}
 		current = current->next;
 	}
 	return (count);
