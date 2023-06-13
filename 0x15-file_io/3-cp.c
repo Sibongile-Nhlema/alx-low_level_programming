b20ea2e84ee978aeeb5488a73c2d4aae4503415b
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-06-13 15:41:39 -0700

    rewrote the code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,80 +3,77 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: arguments vector
- * Return: Always 0
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
 
-void copy_file(char *from, char *to);
-int open_file(char *filename, int mode, int permissions);
-
-int main(int argc, char *argv[])
+void error_file(int file_from, int file_to, const char *argv[])
 {
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
+
+int main(int argc, const char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
+
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	copy_file(argv[1], argv[2]);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
+
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
 	return (0);
-}
-
-/**
- * copy_file - copies file
- * @from: pointer
- * @to: pointer
- * Return: nothing
- */
-
-void copy_file(char *from, char *to)
-{
-	int fd_from = open_file(from, O_RDONLY, 0);
-	int fd_to = open_file(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	char buffer[1024];
-	ssize_t bytes_read, bytes_written;
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", to);
-			exit(99);
-		}
-	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
-}
-
-/**
- * open_file - opens file
- * @filename: filename
- * @mode: mode
- * @permissions: permissions
- * Return: 1 on sucess or -1 on error
- */
-
-int open_file(char *filename, int mode, int permissions)
-{
-	int fd = open(filename, mode, permissions);
-	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
-		exit(98);
-	}
-	return (fd);
 }
