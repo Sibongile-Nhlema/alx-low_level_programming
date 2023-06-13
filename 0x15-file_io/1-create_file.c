bcfe4374b606f39e213e5b7b512649644159e7da
Sibongile-Nhlema <sibongile.nhlema@gmail.com>
2023-06-13 13:46:47 -0700

    task 1 corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,26 +12,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fp;
-	int ret;
+	int fd, ret, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fp = fopen(filename, "w");
-	if (fp == NULL)
-		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	if (fd == -1)
+		return (text_content == NULL ? 1 : -1);
 
 	if (text_content != NULL)
 	{
-		ret = fprintf(fp, "%s", text_content);
-		if (ret < 0)
+		while (text_content[len] != '\0')
+			len++;
+
+		ret = write(fd, text_content, len);
+		if (ret == -1)
 		{
-			fclose(fp);
+			close(fd);
 			return (-1);
 		}
 	}
-
-	fclose(fp);
+	close(fd);
 	return (1);
 }
