b4baa3a528e6ef98a576eb151f708c6451784878
Sibongile <sibongile.nhlema@gmail.com>
2023-08-30 12:24:22 -0700

    Task 2. Function like strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0') /*base case. end char*/
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
