73ff19829dec9c56af7c8320e63fc2f5aab0654c
Sibongile <sibongile.nhlema@gmail.com>
2023-08-30 10:12:33 -0700

    Completed Task 0. prints string and /n

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: imputted string
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+	}
+	_putchar('\n');
+}
