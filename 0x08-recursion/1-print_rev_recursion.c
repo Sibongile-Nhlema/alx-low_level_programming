80c262d4eb80c5867ab620aa0d247995a3c9783e
Sibongile <sibongile.nhlema@gmail.com>
2023-08-30 11:42:40 -0700

    task 1. function to reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: imputted string
+ *
+ * Return: reversed string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{ /*reverse of task 0 code*/
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
