3f3322fda04480b5b12200c02a3b816b54afcbb1
Sibongile <sibongile.nhlema@gmail.com>
2023-09-01 16:21:43 -0700

    Task 5. Finally works. Coins counted

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int countCoins(int num);
+
 /**
  * main - prints min num of coins for change
  * @argc: argument count
@@ -13,39 +15,48 @@
 int main(int argc, char *argv[])
 {
 	int num;
-	int cents[] = {25, 0, 5, 2, 1};
 	int coins;
-	int i;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	coins = 0;
+
 	num = atoi(argv[1]);
+	coins = countCoins(num);
+
+	printf("%d\n", coins);
+	return (0);
+}
+
+/**
+ * countCoins - counts coins
+ * @num: given number
+ *
+ * Return: number of coins
+ */
+
+int countCoins(int num)
+{
+	int money[] = {25, 10, 5, 2, 1};
+	int coins = sizeof(money) / sizeof(money[0]);
+	int result = 0;
+	int i;
 
 	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	else if (num == 0)
-	{
-		printf("0\n");
-		return (0);
-	}
 
-
-	for (i = 0; i < 5 && num >= 0; i++)
+	for (i = 0; i < coins; i++)
 	{
-		if (num >= cents[i])
+		while (num >= money[i])
 		{
-			coins += num / cents[i];
-			num %= cents[i];
+			result++;
+			num -= money[i];
 		}
 	}
-
-	printf("%d\n", coins);
-	return (0);
+	return (result);
 }
