b6a18a856cb25b78f35ea3c814d245f6d519b8be
Sibongile <sibongile.nhlema@gmail.com>
2023-09-01 04:40:30 -0700

    created files ans README

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that copies the string
+ * @src: copy from
+ * @dest: copy to
+ * Return: string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+}
