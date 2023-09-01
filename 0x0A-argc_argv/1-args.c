c4cbd591cb7d7f8f67034f103a546c5409a75431
Sibongile <sibongile.nhlema@gmail.com>
2023-09-01 08:15:29 -0700

    task 1. program that counts arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  prints the number of arguments passed into it
+ * @argc: argument count
+ * @argv: agrument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1); /* exclude program name */
+
+	return (0);
+}
