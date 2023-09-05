6a16a7de2c7ff7398282b3c366fba30264939517
Sibongile <sibongile.nhlema@gmail.com>
2023-09-05 12:15:07 -0700

    task 6. function splits strings into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,81 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int countWords(char *str);
+
+/**
+ * strtow -  function that splits a string into words
+ * @str: string
+ *
+ * Return: number of words
+ */
+
+char **strtow(char *str)
+{
+	int word_count = countWords(str);
+	int i, j, k, l;
+	int word_length;
+	char **words = malloc((word_count + 1) * sizeof(char *));
+
+	if (words == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+	k = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ')
+		{
+			word_length = 0;
+			while (str[i] != ' ' && str[i] != '\0')
+			{
+				word_length++;
+				i++;
+			}
+			words[j] = malloc((word_length + 1) * sizeof(char));
+
+			if (words[j] == NULL)
+			{
+				for (l = 0; l < j; l++)
+					free(words[l]);
+				free(words);
+				return (NULL);
+			}
+
+			for (k = 0; k < word_length; k++)
+				words[j][k] = str[i - word_length + k];
+
+			words[j][k] = '\0';
+			j++;
+		}
+		else
+			i++;
+	}
+	words[j] = NULL;
+	return (words);
+}
+
+/**
+ * countWords - counts words
+ * @str: string
+ * Return: number of words
+ */
+
+int countWords(char *str)
+{
+	int word_count = 0;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+		{
+			word_count++;
+		}
+		i++;
+	}
+	return (word_count);
+}
