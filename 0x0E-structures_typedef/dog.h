6a2fb100a40cc0b759fc78c12ffd8e7b8866582d
Sibongile <sibongile.nhlema@gmail.com>
2023-09-11 12:56:24 -0700

    fixed typedef error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,18 +9,12 @@
  * Description: longer details
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t -typedef for struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
