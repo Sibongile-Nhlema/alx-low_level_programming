5e621c007ed4063ca8ec83bfe070c8afe5d34653
Sibongile <sibongile.nhlema@gmail.com>
2023-09-11 08:43:28 -0700

    task 2 corrected

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,8 +1,4 @@
-#include "main.h"
-#include <unistd.h>
 #include <stdio.h>
-
-int _putchar(char c);
 
 /**
  * main - prints file name
@@ -11,28 +7,6 @@ int _putchar(char c);
 
 int main(void)
 {
-	const char *filename = __FILE__;
-	int i = 0;
-
-	while (filename[i] != '\0')
-	{
-		_putchar(filename[i]);
-		i++;
-	}
-
-	_putchar('\n');
-
+	printf("%s\n", __FILE__);
 	return (0);
-}
-
-/**
- * _putchar - write single char to stdout
- * @c: char
- *
- * Return: always 0
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
