ec2f8f325ad6c163248c69e40d793b86aff33f7c
Sibongile <sibongile.nhlema@gmail.com>
2023-10-02 18:23:37 +0000

    tried to change permissions

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,23 +13,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, prnt, length;
+	int fd, prnt, length = 0;
 
 	/* check if file exists first */
 	if (filename == NULL) /*filename does not exist*/
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		while (text_content[length] != '\0')
 			length++;
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_WRONLY | O_RDONLY | O_APPEND);
 	prnt = write(fd, text_content, length);
 
 	if (fd == -1 || prnt == -1) /* if user does not have permisions*/
 		return (-1);
 	close(fd);
+
 	return (1);
 
 }
