00686ff923d1898f8081920d9c1a98729388e5a5
Sibongile <sibongile.nhlema@gmail.com>
2023-11-09 09:40:21 -0800

    fixed betty errors

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,17 +17,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
-
 	if (*h == NULL)
 	{
 		*h = new_node;
 		return (new_node);
 	}
-
 	temp = *h;
 	if (idx == 0)
 	{
@@ -36,7 +33,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-
 	while (temp != NULL)
 	{
 		if (count == idx - 1)
