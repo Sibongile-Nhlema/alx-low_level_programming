f1eb062ee3fe0a15f26661cb18b1cfec54925ff2
Sibongile <sibongile.nhlema@gmail.com>
2023-11-09 09:22:25 -0800

    added more edge cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,6 +17,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	if (h == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->n = n;
 	temp = *h;
 	while (temp != NULL)
