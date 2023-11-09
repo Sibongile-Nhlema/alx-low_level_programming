8fc2c2ba34a10310827a30d8227535bbe781a7ab
Sibongile <sibongile.nhlema@gmail.com>
2023-11-09 11:03:20 -0800

    completed task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,4 +9,42 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *temp, *to_delete;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	temp = *head;
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+	while (temp != NULL)
+	{
+		if (count == index - 1)
+		{
+			if (temp->next != NULL)
+			{
+				to_delete = temp->next;
+				temp->next = temp->next->next;
+				if (temp->next != NULL)
+					temp->next->prev = temp;
+				free(to_delete);
+				return (1);
+
+			}
+			else
+			{
+				return (-1);
+			}
+		}
+		temp = temp->next;
+		count++;
+	}
+	return (-1);
 }
--- 0x17-doubly_linked_lists/8-main.c
+++ 0x17-doubly_linked_lists/8-main.c
@@ -14,7 +14,7 @@ int main(void)
 
 	head = NULL;
 	add_dnodeint_end(&head, 0);
-	add_dnodeint_end(&head, 1);delete_dnodeint_at_index(&head, 0);lll
+	add_dnodeint_end(&head, 1);
 	add_dnodeint_end(&head, 2);
 	add_dnodeint_end(&head, 3);
 	add_dnodeint_end(&head, 4);
