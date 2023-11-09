12acefafff90b32e4179c22adcac10af8d591227
Sibongile <sibongile.nhlema@gmail.com>
2023-11-09 07:50:48 -0800

    fixed silly mistake

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *node;
 
 	node = head;
-	while (head != NULL)
+	while (node != NULL)
 	{
 		if (index == count)
 		{
