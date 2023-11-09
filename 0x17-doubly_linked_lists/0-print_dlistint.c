55fa775ab5ab887b39af8a126e450c9585e935dd
Sibongile <sibongile.nhlema@gmail.com>
2023-11-09 04:05:24 -0800

    changed data type for counter

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,11 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 	const dlistint_t *current = h;
 
 	if (current == NULL)
-	{
 		return (0);
-	}
 
 	while (current != NULL)
 	{
