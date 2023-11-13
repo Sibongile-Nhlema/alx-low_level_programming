14119cd6ad1c6f90640c584ae44771dcee828b57
Sibongile <sibongile.nhlema@gmail.com>
2023-11-13 02:19:25 -0800

    completed task 2

---
+++ 0x18-dynamic_libraries/math_operations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - addes two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Sum of two numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Difference between two numbers
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: Product of two numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides first number by second number
+ * @a: first number
+ * @b: second number
+ * Return: Quotients of numbers
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - finds the remainder
+ * @a: first number
+ * @b: second number
+ * Return: remainder
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
