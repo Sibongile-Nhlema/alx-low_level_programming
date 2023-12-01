be7abfaa4eb82f2435a5dead6117f4fc7ff652cc
Sibongile <sibongile.nhlema@gmail.com>
2023-12-01 06:32:37 -0800

    added collision handling

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,23 +14,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_element;
+	hash_node_t *new_element, *current;
+
+	if (key == NULL || ht == NULL || *key == '\0')
+		return (0);
 
 	new_element = malloc(sizeof(hash_node_t));
 	if (new_element == NULL)
 		return (0);
 
-	if (key == NULL || ht == NULL || *key == '\0')
-	{
-		free(new_element);
-		return (0);
-	}
 	index = key_index((const unsigned char *)key, ht->size);
+
+	current = ht->array[index];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(new_element);
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
 
 	new_element->key = strdup(key);
 	new_element->value = strdup(value);
 	new_element->next = ht->array[index];
-
 
 	ht->array[index] = new_element;
 
