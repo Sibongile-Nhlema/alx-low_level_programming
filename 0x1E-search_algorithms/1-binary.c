8e5fc64460b482cab8b1cdf9895fde1e6ce5219a
Sibongile <sibongilenhlema@gmail.com>
2024-03-11 09:27:51 -0700

    completed task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+
+/**
+ * binary_search -  searches for a value in an array of integers
+ * using the Linear search algorithm
+ * @array: pointer to the first element of the array
+ * @size:  the number of elements in array
+ * @value: the value to search for
+ * Return: first index where value is located or -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0, right = size - 1, mid, index;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Searching subarray: ");
+		for (index = left; index <= right; index++)
+			printf("%d ", array[index]);
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
