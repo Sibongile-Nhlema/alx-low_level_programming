5335141153f463543edaf15a5ed8abb13b50af50
Sibongile <sibongilenhlema@gmail.com>
2024-03-12 10:22:47 -0700

    resolved the betty errors

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,28 +11,24 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-    size_t jump_step = sqrt(size);
-    size_t i, low = 0, high = jump_step;
+	size_t jump_step = sqrt(size);
+	size_t i, low = 0, high = jump_step;
 
-    if (array == NULL)
-        return (-1);
-
-    printf("Value checked array[%lu] = [%d]\n", low, array[low]);
-    while (high < size && array[high] < value)
-    {
-        printf("Value checked array[%lu] = [%d]\n", high, array[high]);
-        low = high;
-        high += jump_step;
-    }
-
-    printf("Value found between indexes [%lu] and [%lu]\n", low, high);
-
-    for (i = low; i <= high && i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-
-    return (-1);
+	if (array == NULL)
+		return (-1);
+	printf("Value checked array[%lu] = [%d]\n", low, array[low]);
+	while (high < size && array[high] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", high, array[high]);
+		low = high;
+		high += jump_step;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", low, high);
+	for (i = low; i <= high && i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
