7194c6a31291ef46d0f8356fb4498bd793fc2a3d
Sibongile <sibongilenhlema@gmail.com>
2024-03-14 00:59:17 -0700

    changed conditions

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -60,7 +60,7 @@ int advanced_binary_recursive(int *array, size_t left,
 		else if (array[mid] < value)
 			return (advanced_binary_recursive(array, mid + 1, right, value));
 		else if (array[mid] > value)
-			return (advanced_binary_recursive(array, left, mid - 1, value));
+			return (advanced_binary_recursive(array, left, mid, value));
 		else
 			return (-1);
 	}
