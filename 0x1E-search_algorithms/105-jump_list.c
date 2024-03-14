ce22323c43754f74246b8fed343831fe7042c21c
Sibongile <sibongilenhlema@gmail.com>
2024-03-14 08:55:06 -0700

    working on the out odf bounds issues

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,26 +12,47 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	listint_t *current, *tmp;
-	size_t i, step = sqrt(size);
+	listint_t *current, *tmp, *prev, *tmp_prev;
+	size_t step = sqrt(size);
 
 	if (list == NULL)
 		return (NULL);
-
-	printf("step = %lu\nvalue = %d\n", step, value);
 	/*check list */
-	i = 0;
-	while (i <= step)
+	current = list;
+	while (current)
 	{
-		tmp = list;
-		current = tmp->next;
-		i++;
-	}
-	printf("current = %d\n", current->n);
-	while (current->next != NULL)
-	{
+		/* check if current's index is a factor of step */
+		if (current->index != 0 && current->index % step == 0)
+		{
+			printf("Value checked at index [%lu] = [%d]\n",
+			       current->index, current->n);
+			/* check is value is present between the steps*/
+			if (value <= current->n)
+			{
+				printf("Value found between indexes [%lu] and [%lu]\n",
+				       (current->index - step), current->index);
+				/* find previous node */
+				prev = list;
+				while  (prev->next != NULL && prev->index != (current->index - step))
+				{
+					tmp_prev = prev;
+					prev = tmp_prev->next;
+				}
+				/* perform linear search */
+				while (prev->n <= value)
+				{
+					printf("Value checked at index [%lu] = [%d]\n",
+					       prev->index, prev->n);
+					if (prev->n == value)
+						return (prev);
+					tmp_prev = prev;
+					prev = tmp_prev->next;
+				}
+				return (prev);
+			}
+		}
+		/* check for value*/
 		tmp = current;
-		printf("Value checked at index [%lu] = [%d]\n", i, current->n);
 		current = tmp->next;
 	}
 	return (list);
